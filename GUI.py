dde9659bec142f218cacf8910c131d9a8e3e5d5f
Shubham Somani <shubhamsomani92@gmail.com>
2013-06-12 17:29:50 +0530

    Changes after fitter conf.t
    Includes parsing of description and notes also from conf.t and also a basic check function

--- GUI.py
+++ GUI.py
@@ -17,6 +17,9 @@ config_parser.write()
 #reading from the configuration file
 parameters=config_parser.read()
 
+#This is the array of all fields in the GUI.It is declared globally so that other functions can access values entered in the fields.
+input_text=[[None for x in range(100)] for y in range(300)]
+
 class Page(scrolled.ScrolledPanel):
     def __init__(self, parent):
         scrolled.ScrolledPanel.__init__(self, parent,style = wx.TAB_TRAVERSAL|wx.SUNKEN_BORDER)
@@ -33,11 +36,9 @@ class MyApp(wx.Frame):
         # Declaration of variables
         
         label={}
-        input_text={}
         inputsizer={}
         pagesizer={}
         page={}
-        id_number=0
 
 
         # Creation of sizer for panel and notebook
@@ -59,38 +60,43 @@ class MyApp(wx.Frame):
             pagesizer[section_index]=wx.BoxSizer(wx.VERTICAL)
 
             # The second loop is for information inside each page.
-            # It is incremented by 3 every time as each set is of the form {parameter name,data type,default value}
+            # It is incremented by 6 every time as each set is of the form {parameter name, data type, range, default value, description, notes}
 
-            for parameter_index in range(1,len(parameters[section_index]),3):
+            for parameter_index in range(1,len(parameters[section_index]),6):
 
                 #Parameter name
 
                 label_text=parameters[section_index][parameter_index]
-                label[id_number]= wx.StaticText(page[section_index],wx.ID_ANY,label_text)
+                label[parameter_index]= wx.StaticText(page[section_index],wx.ID_ANY,label_text)
 
-                # Data Type
+                # Data Type and default value
 
                 if(parameters[section_index][parameter_index+1].strip()=="Boolean feature macro."):
-                    input_text[id_number]=wx.CheckBox(page[section_index], wx.ID_ANY, '', (10, 10))
+                    input_text[section_index][parameter_index]=wx.CheckBox(page[section_index], wx.ID_ANY, '', (10, 10))
                 else:
-                    if("The default for this field is " in parameters[section_index][parameter_index+2]):
-                        temp=parameters[section_index][parameter_index+2].split("The default for this field is ")
+                    if("The default value is " in parameters[section_index][parameter_index+3]):
+                        temp=parameters[section_index][parameter_index+3].split("The default value is ")
                         temp2=temp[1]
-                        final=temp2.split(".")
-                        input_text[id_number]=wx.TextCtrl(page[section_index],wx.ID_ANY,final[0])
+                        if("." in temp2):
+                            final=temp2.split(".")
+
+                        #CONFIGURE_INIT_TASK_NAME is an exception as its default value has commas in it.
+                        if("," in temp2 and label_text!="CONFIGURE_INIT_TASK_NAME"):
+                            final=temp2.split(",")
+                        input_text[section_index][parameter_index]=wx.TextCtrl(page[section_index],wx.ID_ANY,final[0])
                     else:
-                        input_text[id_number]=wx.TextCtrl(page[section_index],wx.ID_ANY,'Enter Value')
+                        input_text[section_index][parameter_index]=wx.TextCtrl(page[section_index],wx.ID_ANY,'Enter Value')
 
                 # Tooltip showing default value
 
-                label[id_number].SetToolTip(wx.ToolTip(parameters[section_index][parameter_index+2]))
+                label[parameter_index].SetToolTip(wx.ToolTip(parameters[section_index][parameter_index+3]))
 
                 # Sizers
 
-                inputsizer[id_number]=wx.BoxSizer(wx.HORIZONTAL)
-                inputsizer[id_number].Add(label[id_number],0,wx.ALL,10)
-                inputsizer[id_number].Add(input_text[id_number],1,wx.ALL|wx.EXPAND,10)
-                pagesizer[section_index].Add(inputsizer[id_number],0,wx.ALL|wx.EXPAND,10)
+                inputsizer[parameter_index]=wx.BoxSizer(wx.HORIZONTAL)
+                inputsizer[parameter_index].Add(label[parameter_index],0,wx.ALL,10)
+                inputsizer[parameter_index].Add(input_text[section_index][parameter_index],1,wx.ALL|wx.EXPAND,10)
+                pagesizer[section_index].Add(inputsizer[parameter_index],0,wx.ALL|wx.EXPAND,10)
             page[section_index].SetSizer(pagesizer[section_index])
         topsizer.Add(notebook,1,wx.EXPAND)
         self.panel.SetSizer(topsizer)
@@ -109,7 +115,8 @@ class MyApp(wx.Frame):
         file1.Append(101, '&Open', 'Open the existing configuration file')
         file1.Append(102, '&Save\tCtrl+S', 'Save the current configurations')
         file1.AppendSeparator()
-        file1.Append(103, '&Quit\tCtrl+Q', 'Quit the Application')
+        file1.Append(103, '&Check', 'Checks the format of all values')
+        file1.Append(104, '&Quit\tCtrl+Q', 'Quit the Application')
 
         #Appending items to menu
         menubar.Append(file1, '&File')  
@@ -118,11 +125,33 @@ class MyApp(wx.Frame):
         self.SetMenuBar(menubar)
 
         #Event Handler
-        self.Bind(wx.EVT_MENU,self.OnQuit,id=103)
-        self.Bind(wx.EVT_MENU,self.OnOpen,id=101)        
+        self.Bind(wx.EVT_MENU,self.OnOpen,id=101)
+        self.Bind(wx.EVT_MENU,self.OnCheck,id=103)
+        self.Bind(wx.EVT_MENU,self.OnQuit,id=104)
         
     def OnQuit(self,event):
         self.Close()
+
+    def OnCheck(self, event):
+        #INCOMPLETE
+        message="The following parameters are not given desired values "
+        number_of_sections=len(parameters)
+        for section_index in xrange(number_of_sections):
+            for parameter_index in range(1,len(parameters[section_index]),6):
+                #print to see if correct parameters reach this function.
+                #label_text=parameters[section_index][parameter_index]
+                #print label_text+"   "+(str)(parameter_index)
+                #print input_text[section_index][parameter_index].GetValue()
+                if(parameters[section_index][parameter_index+1].strip()!="Boolean feature macro."):
+
+                    # Conditions not correct. Have to be changed according to ranges once they are updated.
+                    if(input_text[section_index][parameter_index].GetValue()=="0" or input_text[section_index][parameter_index].GetValue()=="Enter Value"):
+                        continue
+                    else:
+                        message=message+"\n"+parameters[section_index][parameter_index]
+        dial = wx.MessageDialog(None,message, 'Not Compatible Data Types', wx.OK)
+        dial.ShowModal()
+
 
     def OnOpen(self, event):
         dlg = wx.FileDialog(self,"Choose a file",os.getcwd(),"","*.*",wx.OPEN)
--- text/text_parser.py
+++ text/text_parser.py
@@ -7,9 +7,12 @@ def return_parameters():
 
     data=open("conf.t")
     contents=data.readlines()
+
+    # Variable Declarations
     list=[]
-    flag=0
+    count_of_sections=0
     start=0
+
     for index in range(0,len(contents)):
         if "@section" in contents[index]:               
             s=contents[index].split("@section")
@@ -18,24 +21,33 @@ def return_parameters():
         if "@section" in contents[index] and start==1:               
             s=contents[index].split("@section")
             #print s[1].strip()
-            if flag==1:
+            if count_of_sections>0:
                 list.append("end of subsection")
             list.append(s[1].strip())
-            flag=1
-        if "@findex" in contents[index] and flag==1:
+            count_of_sections=count_of_sections+1
+
+        # Parsing the name of the parameter
+        if "@findex" in contents[index] and count_of_sections>0:
             s=contents[index].split("@findex")
-            #print s[1].strip()
             list.append(s[1].strip())
             while(contents[index].strip()!="@end table"):
                 index=index+1
+
+                # Data type
                 if(contents[index].strip()=="@item DATA TYPE:"):
                     #print contents[index+1]
-                    list.append(contents[index+1])
+                    list.append(contents[index+1].strip())
+
+                # Range
+                if(contents[index].strip()=="@item RANGE:"):
+                    list.append(contents[index+1].strip())
+
+                # Default Value
                 if(contents[index].strip()=="@item DEFAULT VALUE:"):
-                    index=index+1
                     input_string=""
+                    index=index+1
                     while(contents[index].strip()!="@end table"):
-                        input_string=input_string+contents[index].strip()
+                        input_string=input_string+" "+contents[index].strip()
                         index=index+1
                     if("@code{" in input_string):
                         string=input_string.split("@code{")
@@ -45,9 +57,32 @@ def return_parameters():
                         list.append(string_after_operations)
                     else:
                         list.append(input_string)
+ 
+            # Description  
+            while (contents[index].strip()!="@subheading DESCRIPTION:"):
+                index=index+1
+
+            description=""
+            index=index+1
+            while(contents[index].strip()!="@subheading NOTES:"):
+                description=description+" "+contents[index].strip()
+                index=index+1
+            list.append(description)
+
             
-    #----------- conf.t read complete, processing starts --------------------
-            
+            # Notes
+            notes=""
+            index=index+1
+            while(contents[index].strip()!="@c"):
+                notes=notes+" "+contents[index].strip()
+                index=index+1
+            list.append(notes)
+
+
+    #----------- at the end of read parameters have the following format--------------------------------------------------------------------------------------
+    # section name1,parameter11,parmeter12....parameter1n,end of subsection,section name2,parameter21,parmeter22....parameter2n,end of subsection...and so on.                    
+    #----------- conf.t read complete, processing starts -----------------------------------------------------------------------------------------------------       
+
     parameters=[]
     index=0
     section=0
@@ -65,13 +100,13 @@ def return_parameters():
     #print parameters
     return parameters
 
-return_parameters()
+#return_parameters()
 
     #---------------------- end of processing ------------------------------
     #          The format of the parameters after processing is as follows
-    #   {name of section-1, {parameter-1,data type-1,default value-1},.......,{parameter-n,data type-n,default value-n}}
+    #   {name of section-1, {parameter-1,data type-1,range-1,default value-1,description-1,notes-1},.......,{parameter-n,data type-n,range-n,default value-n,description-n,notes-n}}
     #   .
     #   .
     #   .
-    #   {name of section-m, {parameter-1,data type-1,default value-1},.......,{parameter-n,data type-n,default value-n}}
+    #   {name of section-m, {parameter-1,data type-1,range-1,default value-1,description-1,notes-1},.......,{parameter-n,data type-n,range-n,default value-n,description-n,notes-n}}
     
