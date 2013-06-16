09bc7561a6b39c5cb00ab1c4a1ed3c446972a65c
Shubham Somani <shubhamsomani92@gmail.com>
2013-06-17 03:13:07 +0530

    Adding Save and Load functionality

--- GUI.py
+++ GUI.py
@@ -2,23 +2,22 @@
 # Author: Shubham Somani
 
 #!/usr/bin/python
-import config_parser
 
+from text import text_parser
 import wx
 import  wx.lib.scrolledpanel as scrolled
 import os,sys
 import random
+import ConfigParser
 
+# List of parameters to be read from file
 parameters={}
 
-#creating a configuration file
-config_parser.write()
-
-#reading from the configuration file
-parameters=config_parser.read()
+#reading from the text parser
+parameters=text_parser.return_parameters()
 
 #This is the array of all fields in the GUI.It is declared globally so that other functions can access values entered in the fields.
-input_text=[[None for x in range(100)] for y in range(300)]
+input_text=[[None for x in range(100)] for y in range(120)]
 
 class Page(scrolled.ScrolledPanel):
     def __init__(self, parent):
@@ -112,26 +111,63 @@ class MyApp(wx.Frame):
         edit=wx.Menu()
         help1=wx.Menu()
 
-        #Items of file
-        file1.Append(101, '&Open', 'Open the existing configuration file')
-        file1.Append(102, '&Save\tCtrl+S', 'Save the current configurations')
-        file1.AppendSeparator()
-        file1.Append(103, '&Check', 'Checks the format of all values')
-        file1.Append(104, '&Quit\tCtrl+Q', 'Quit the Application')
-
         #Appending items to menu
         menubar.Append(file1, '&File')  
         menubar.Append(edit, '&Edit') 
         menubar.Append(help1, '&Help')
         self.SetMenuBar(menubar)
 
+        #Items of file
+        file1.Append(101, '&Open', 'Open the existing configuration file')
+        file1.Append(102, '&Save\tCtrl+S', 'Save the current configurations')
+        file1.AppendSeparator()
+        file1.Append(103, '&Check', 'Checks the format of all values')
+        file1.Append(104, '&Load', 'Load the saved state')
+        file1.Append(105, '&Quit\tCtrl+Q', 'Quit the Application')
+
         #Event Handler
         self.Bind(wx.EVT_MENU,self.OnOpen,id=101)
+        self.Bind(wx.EVT_MENU,self.OnSave,id=102)
         self.Bind(wx.EVT_MENU,self.OnCheck,id=103)
-        self.Bind(wx.EVT_MENU,self.OnQuit,id=104)
-        
-    def OnQuit(self,event):
-        self.Close()
+        self.Bind(wx.EVT_MENU,self.OnLoad,id=104)
+        self.Bind(wx.EVT_MENU,self.OnQuit,id=105)
+
+    def OnOpen(self, event):
+        dlg = wx.FileDialog(self,"Choose a file",os.getcwd(),"","*.*",wx.OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+                path = dlg.GetPath()
+                mypath = os.path.basename(path)
+                self.SetStatusText("You selected: %s" %mypath)
+        dlg.Destroy()
+
+    def OnSave(self, event):
+
+        index=0
+        #using python's config parser
+        config=ConfigParser.RawConfigParser()
+
+        number_of_sections=len(parameters)
+        section_name="start"
+        config.add_section(section_name)
+
+        for section_index in xrange(number_of_sections):
+            config.set(section_name,str(index),'start of section')
+            index=index+1
+            for parameter_index in range(1,len(parameters[section_index]),6):
+                config.set(section_name,str(index),input_text[section_index][parameter_index].GetValue())
+                index=index+1
+
+        #Storing the length of index, to be used while loading values.
+        config.add_section("Length of Index")
+        config.set("Length of Index",str(0),index)
+
+        #writing into configuration.ini
+        with open('configuration.ini','wb') as configfile:
+            config.write(configfile)
+
+        dial = wx.MessageDialog(None,'Values Saved', 'RTEMS', wx.OK)
+        dial.ShowModal()
+
 
     def OnCheck(self, event):
         #INCOMPLETE
@@ -153,14 +189,41 @@ class MyApp(wx.Frame):
         dial = wx.MessageDialog(None,message, 'Not Compatible Data Types', wx.OK)
         dial.ShowModal()
 
+    def OnLoad(self,event):
+        #Method to read from configuration.ini
 
-    def OnOpen(self, event):
-        dlg = wx.FileDialog(self,"Choose a file",os.getcwd(),"","*.*",wx.OPEN)
-        if dlg.ShowModal() == wx.ID_OK:
-                path = dlg.GetPath()
-                mypath = os.path.basename(path)
-                self.SetStatusText("You selected: %s" %mypath)
-        dlg.Destroy()
+        config=ConfigParser.RawConfigParser()
+
+        config.read('configuration.ini')
+        section_name="start"
+        section_index=-1
+        index=int(config.get("Length of Index",str(0)))
+
+        for i in xrange(index):
+            temp=config.get("start",str(i))
+
+            if(temp=='start of section'):
+                section_index=section_index+1
+                parameter_index=1
+                continue
+            if(parameters[section_index][parameter_index+1].strip()=="Boolean feature macro."):
+                if(temp=="True"):
+                    input_text[section_index][parameter_index].SetValue(1)
+                else:
+                    input_text[section_index][parameter_index].SetValue(0)
+                parameter_index=parameter_index+6
+                continue
+
+            input_text[section_index][parameter_index].SetValue(temp)
+            parameter_index=parameter_index+6
+
+        dial = wx.MessageDialog(None,'Values Loaded', 'RTEMS', wx.OK)
+        dial.ShowModal()
+
+    def OnQuit(self,event):
+        self.Close()
+
+
 
 #Main Class
 class RTEMS_Configuration_GUI(wx.App):
